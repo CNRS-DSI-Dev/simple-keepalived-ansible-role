159b6f039318195eed7d9acbe83541e3c54a002d
Gilian GAMBINI <gilian.gambini@dsi.cnrs.fr>
2020-06-02 11:36:16 +0200

    feat: Optional custom check script by virtual server. Add check_ldaps.sh.

---
+++ files/check_ldaps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# GGA 22/11/2018
+
+#Make anonymous bind that return "no object found" error code 32
+output=`LDAPTLS_REQCERT=never ldapsearch -x -H "ldaps://$1:$2" -b "" 2>&1`
+
+#If return code is 32, LDAP works :)
+if [[ $? = 32 ]]
+then
+        exit 0
+else
+        exit 1
+fi
+
