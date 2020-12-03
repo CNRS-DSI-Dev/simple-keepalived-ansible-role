5f8ea2955c941ca103fe8135bdf4d4d1b752f153
Gilian GAMBINI <gilian.gambini@dsi.cnrs.fr>
2020-12-03 14:25:08 +0100

    fix: Add option search max time in check_ldaps.sh

--- files/check_ldaps.sh
+++ files/check_ldaps.sh
@@ -2,7 +2,7 @@
 # GGA 22/11/2018
 
 #Make anonymous bind that return "no object found" error code 32
-output=`LDAPTLS_REQCERT=never ldapsearch -x -H "ldaps://$1:$2" -b "" -o nettimeout=10 2>&1`
+output=`LDAPTLS_REQCERT=never ldapsearch -x -H "ldaps://$1:$2" -b "" -o nettimeout=10 -l 10 2>&1`
 
 #If return code is 32, LDAP works :)
 if [[ $? = 32 ]]
